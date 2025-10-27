0bed53637b034e4119142e5f89a5ba9288f9dd8d
Tiger Me <93956183+funnyone7436@users.noreply.github.com>
2025-10-27 11:48:07 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// If you deploy to GitHub Pages at https://<user>.github.io/<repo>/,
-// set GH_PAGES=1 when building and put your repo name below.
-const repoName = 'HealthyGameMainpage' // <— change if you deploy to another repo
-
 export default defineConfig({
   base: '/Rain-In-Halloween/',
   plugins: [react()],
